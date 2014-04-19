c78166fd8c1942b4b2bf3386094f749cc00c183a
zeMirco <mirco.zeiss@gmail.com>
2014-04-19 09:33:23 +0200

    update test app nd use express 4.x

--- test/app/app.js
+++ test/app/app.js
@@ -4,12 +4,18 @@
  */
 
 var express = require('express');
+var favicon = require('static-favicon');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var cookieSession = require('cookie-session');
+var bodyParser = require('body-parser');
+var csrf = require('csurf');
+var errorHandler = require('errorhandler');
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var lockitUtils = require('lockit-utils');
-
 var Login = require('../../index.js');
 
 function start(config) {
@@ -21,27 +27,31 @@ function start(config) {
   // set basedir so views can properly extend layout.jade
   app.locals.basedir = __dirname + '/views';
 
-// all environments
+  // all environments
   app.set('port', process.env.PORT || config.port || 3000);
-  app.set('views', __dirname + '/views');
+  app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
   // make JSON output simpler for testing
   app.set('json spaces', 0);
-  app.use(express.favicon());
-  app.use(express.urlencoded());
-  app.use(express.json());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+
+  app.use(favicon());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded());
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
   if (config.useSessionStore) {
-    console.log('using session store');
-    app.use(express.session());
+    app.use(session({
+      secret: 'this is my super secret string'
+    }));
   } else {
-    app.use(express.cookieSession());
+    app.use(cookieSession({
+      secret: 'this is my super secret string'
+    }));
   }
 
   if (config.csrf) {
-    app.use(express.csrf());
+    app.use(csrf());
     app.use(function(req, res, next) {
 
       var token = req.csrfToken();
@@ -53,17 +63,15 @@ function start(config) {
     });
   }
 
-  // use forgot password middleware with testing options
   var db = lockitUtils.getDatabase(config);
   var adapter = require(db.adapter)(config);
-  var login = new Login(app, config, adapter);
+  var login = new Login(config, adapter);
 
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(login.router);
 
-// development only
+  // development only
   if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+    app.use(errorHandler());
   }
 
   app.get('/', routes.index);
