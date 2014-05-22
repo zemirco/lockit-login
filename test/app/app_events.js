51c05065402aa507270e2040c2de3301bf89f9d7
zeMirco <mirco.zeiss@gmail.com>
2014-05-22 20:37:12 +0200

    add events tests for two factor auth

--- test/app/app_events.js
+++ test/app/app_events.js
@@ -7,6 +7,7 @@ var superagent = require('superagent');
 var cookieParser = require('cookie-parser');
 var cookieSession = require('cookie-session');
 var should = require('should');
+var totp = require('notp').totp;
 var utils = require('lockit-utils');
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -55,6 +56,26 @@ app_two.get('/users', user.list);
 app_two.use(login_two.router);
 http.createServer(app_two).listen(app_two.get('port'));
 
+// create third app for two-factor authentication
+var config_three = JSON.parse(JSON.stringify(config));
+var app_three = express();
+app_three.locals.basedir = __dirname + '/app/views';
+app_three.set('port', 6502);
+app_three.set('views', __dirname + '/views');
+app_three.set('view engine', 'jade');
+app_three.use(bodyParser.json());
+app_three.use(bodyParser.urlencoded());
+app_three.use(cookieParser());
+app_three.use(cookieSession({
+  secret: 'this is my super secret string'
+}));
+app_three.use(express.static(path.join(__dirname, 'public')));
+var login_three = new Login(config_three, adapter);
+app_three.get('/', routes.index);
+app_three.get('/users', user.list);
+app_three.use(login_three.router);
+http.createServer(app_three).listen(app_three.get('port'));
+
 describe('# event listeners', function() {
 
   before(function(done) {
@@ -64,13 +85,26 @@ describe('# event listeners', function() {
       adapter.find('name', 'event', function(err, user) {
         user.emailVerified = true;
         // save updated user to db
-        adapter.update(user, done);
+        adapter.update(user, function() {
+          // create user for two factor auth
+          adapter.save('auth', 'auth@email.com', 'password', function() {
+            // verify email for auth
+            adapter.find('name', 'auth', function(err, user) {
+              user.emailVerified = true;
+              user.twoFactorEnabled = true;
+              user.twoFactorKey = 'qwertz';
+              // save updated user to db
+              adapter.update(user, done);
+            });
+          });
+        });
       });
     });
   });
 
   var agent = superagent.agent();
   var agent_two = superagent.agent();
+  var agent_three = superagent.agent();
 
   describe('POST /login', function() {
 
@@ -89,16 +123,49 @@ describe('# event listeners', function() {
         });
     });
 
-    it.skip('should not emit a "login" event when two-factor auth is enabled', function(done) {
-
+    it('should not emit a "login" event when two-factor auth is enabled', function(done) {
+      var called = false;
+      login_three.on('login', function(user, res, target) {
+        called = true;
+      });
+      agent_three
+        .post('http://localhost:6502/login')
+        .send({login: 'auth', password: 'password'})
+        .end(function(err, res) {
+          // give login event some time
+          setTimeout(function() {
+            called.should.be.false;
+            done();
+          }, 1000);
+        });
     });
 
   });
 
-  describe.skip('POST /login/two-factor', function() {
+  describe('POST /login/two-factor', function() {
 
-    it('should emit a "login" event when two-factor auth is enabled', function(done) {
-
+    it('should emit a "login" event on success', function(done) {
+      login_three.on('login', function(user, res, target) {
+        user.name.should.equal('auth');
+        user.email.should.equal('auth@email.com');
+        target.should.equal('/');
+        done();
+      });
+      agent_three
+        .post('http://localhost:6502/login')
+        .send({login: 'auth', password: 'password'})
+        .end(function(err, res) {
+          process.nextTick(function() {
+            // enter valid token
+            var token = totp.gen('qwertz', {});
+            agent_three
+              .post('http://localhost:6502/login/two-factor')
+              .send({token: token})
+              .end(function(err, res) {
+                res.redirects.should.eql(['http://localhost:6502/']);
+              });
+          });
+        });
     });
 
   });
@@ -153,7 +220,9 @@ describe('# event listeners', function() {
   });
 
   after(function(done) {
-    adapter.remove('event', done);
+    adapter.remove('event', function() {
+      adapter.remove('auth', done);
+    });
   });
 
 });
